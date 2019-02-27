7bce7a73331b4347a4b2843e5018337893694b10
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-02-27 02:39:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,8 +5,8 @@ def square_array(array)
     new_array = []
     new_array.push(item)
     return item
-    puts "hey {#item}
+    puts "item #{item}"
     return new_array
-    puts new_array
+    puts "array value #{new_array}"
   end
 end
