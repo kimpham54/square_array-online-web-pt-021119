aafc27e2762e451c547134100f7f2ca8f44bf4eb
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-02-27 02:41:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
 def square_array(array)
+  new_array = []
   # your code here
   array.each do |item|
     item = item**2
-    new_array = []
     new_array.push(item)
     puts "item #{item}"
-    return new_array
-    puts "array value #{new_array}"
   end
+  puts "array value #{new_array}"
+  return new_array
 end
